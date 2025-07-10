5388a425e901eb8a187bc4d772319561fbbbfa9b
Tritonn204 <tritonn204@gmail.com>
2025-07-09 19:16:12 -0700

    added contextual note to point.rs

--- helioselene-contest/helioselene-contest-src/src/point.rs
+++ helioselene-contest/helioselene-contest-src/src/point.rs
@@ -23,6 +23,19 @@ use crate::{
   field::FieldExtensions
 };
 
+/*
+=================================
+              NOTE
+=================================
+The lazy reduction's bleeding edge was searched for, and seems to have been found in my testing.
+That being said, if there are indeed 0.00n% edge cases where this math fails with point combinations,
+this can be mitigated by forcing addition to reduce_weak() at the end of each add/sub in field.rs at 
+the return points of add() and sub().
+
+My bounds analysis and anectodal spamming of the crate testing via cargo seems to indicate that should
+not be necessary, but as always, I could be wrong as I don't know everything.
+*/
+
 macro_rules! curve {
   (
     $Scalar: ident,
